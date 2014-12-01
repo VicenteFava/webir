0969b0edbe95a98395a793582302c116804a3423
Maite <maitingui@gmail.com>
2014-12-01 02:04:15 -0200

    styles for deals show

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -5,8 +5,12 @@ class DealsController < ApplicationController
     @query = params[:search]
     @search = Sunspot.search(Deal) do |query|  
         query.keywords @query
-        puts params[:search] + "ddddddd"
     end  
     @deals = @search.results
+  end
+
+  def show
+  	@deal = Deal.find(params[:id])
+  	@deal
   end
 end
